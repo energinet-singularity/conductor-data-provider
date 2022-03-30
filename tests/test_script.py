25ee837caa91fddf6e9e22d06273204a0a5721f1
Anders Wittendorff <andersw89@gmail.com>
2022-03-30 09:25:55 +0000

    initial testcases.

--- tests/test_script.py
+++ tests/test_script.py
@@ -4,3 +4,15 @@
 # Dummytest which will always succeed - must be replaced by real tests
 def test_dummy():
     assert True
+
+def test_dd20_cleaner():
+    pass
+
+def test_mrid_map_dict():
+    pass
+
+def test_moat_ets_dd20_map_dict():
+    pass
+
+def combine_conduct_info_to_pandas():
+    pass
