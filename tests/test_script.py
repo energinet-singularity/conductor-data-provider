8c6e835977e55aba92746faf0e240939016dd866
Thomas Pedersen <91196174+stigsberg@users.noreply.github.com>
2022-03-31 12:46:00 +0200

    Test/unit test (#2)
    * Added 2 testcases and tried to include files in test case

--- tests/test_script.py
+++ tests/test_script.py
@@ -2,6 +2,11 @@ import pytest
 import app
 import os
 
+# Needs to be checked to ensure test files are imported correct
+@pytest.fixture
+def valid_files():
+    return [os.path.abspath(os.path.join(dirpath, f)) for dirpath, _, filenames in
+            os.walk(f"{os.path.dirname(os.path.realpath(__file__))}/valid-testdata/") for f in filenames]
 
 # Dummytest which will always succeed - must be replaced by real tests
 def test_dummy():
@@ -26,16 +31,26 @@ def test_dd20_cleaner():
         assert app.my_script.parse_dd20(file=dd20_filepath) == expected_dict
         
     assert True
-
-
+'''
 def test_mrid_map_dict():
-    pass
+    data = ''
+    test_dict = {'d51269gh-25e0-4a11-b32e-bd0fba7af745': 'EEE-FFF-1',
+                     'd51269gh-25e0-4a11-b32e-bd0fba7af747': 'EEE-FFF-2',
+                     'd51269gh-25e0-4a11-b32e-bd0fba7af751': 'GGG-HHH',
+                     'd51269gh-25e0-4a11-b32e-bd0fba7af757': 'CCC-DDD',
+                     'd51269gh-25e0-4a11-b32e-bd0fba7af759': 'III-ÆØÅ',
+                     'd51269gh-25e0-4a11-b32e-bd0fba7af760': 'ASK-ERS'
+                    }
+    assert my_function(data) == test_dict
 
 
 def test_moat_ets_dd20_map_dict():
-    pass
 
+    data2 = ''
+    test_dict2 = {'E_GGG-HHH': 'E_GGGV-HHH'}
 
+    assert my_function2(data2) == test_dict2
+'''
 """
 def combine_conduct_info_to_pandas():
     # CODE:
@@ -63,5 +78,4 @@ def combine_conduct_info_to_pandas():
     # use dict to fill out NAME_MAPPED (warning if mapping name not used)
     # use dict to fill out MRID (error if not possible)
 
-    pass
 """
