db76bed21ee707b6254dac196a2b1cdd2926d1af
Thomas Pedersen <91196174+stigsberg@users.noreply.github.com>
2022-04-07 15:33:15 +0200

    Feature/dict functions (#7)
    * Created 3 functions.
    
    1 to import data from a CSV file. 2 to clean up the dataframe from mrid csv file. 3 Last function converts pandas dataframe to a dictonary
    
    * Imported the functions to test.
    
    Plus general cleanup with variables naming.
    
    * changed to relative paths
    
    * Correcting merge conflict
    
    * Fix test issue after cleaning merge conflict

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,7 +1,6 @@
-import pytest
-import os
-import app.my_script
 import pandas as pd
+import os
+import app.my_script as code
 
 
 # TODO: add negative test with invalid data also
@@ -19,15 +18,45 @@ def test_extract_conducter_data_from_dd20():
 
     dd20_filepath = f"{os.path.dirname(os.path.realpath(__file__))}/valid-testdata/DD20.XLSM"
 
-    dd20_dataframe = app.my_script.parse_excel_sheet_to_dataframe(file_path=dd20_filepath,
-                                                                  sheet_name='Stationsdata',
-                                                                  header_index=1)
+    dd20_dataframe = code.parse_excel_sheet_to_dataframe(file_path=dd20_filepath,
+                                                         sheet_name='Stationsdata',
+                                                         header_index=1)
 
-    resulting_dd20_dataframe = app.my_script.extract_conducter_data_from_dd20(dataframe=dd20_dataframe)
-    print(resulting_dd20_dataframe)
-    print(expected_dd20_dataframe)
+    resulting_dd20_dataframe = code.extract_conducter_data_from_dd20(dataframe=dd20_dataframe)
 
     assert resulting_dd20_dataframe.equals(expected_dd20_dataframe)
+
+
+def test_define_dictonary_from_two_columns_in_a_dataframe():
+
+    DLR_MRID_FILEPATH = f'{os.path.dirname(os.path.realpath(__file__))}/valid-testdata/DLR_MRID.csv'
+    MRID_KEY_NAME = 'LINE_EMSNAME'
+    MRID_VALUE_NAME = 'ACLINESEGMENT_MRID'
+
+    mrid_dataframe = code.parse_csv_file_to_dataframe(DLR_MRID_FILEPATH)
+    TEST_DICTONARY = {'E_EEE-FFF-1':    '66b4596e-asfv-tyuy-5478-bd208f26a446',
+                      'E_EEE-FFF-2':    '66b4596e-asfv-tyuy-5478-bd208f26a447',
+                      'E_GGG-HHH':      '66b4596e-asfv-tyuy-5478-bd208f26a451',
+                      'D_CCC-DDD':      '66b4596e-asfv-tyuy-5478-bd208f26a455',
+                      'C_III-ÆØÅ':      '66b4596e-asfv-tyuy-5478-bd208f26a457',
+                      'C_ASK-ERS':      '66b4596e-asfv-tyuy-5478-bd208f26a459'}
+
+    assert code.define_dictonary_from_two_columns_in_a_dataframe(mrid_dataframe,
+                                                                 MRID_KEY_NAME, MRID_VALUE_NAME) == TEST_DICTONARY
+
+
+def test_define_dictonary_from_two_columns_in_a_dataframe2():
+
+    LIMITS_NAME_FILEPATH = f'{os.path.dirname(os.path.realpath(__file__))}/valid-testdata/Limits_other.xlsx'
+    DD20_DATASHEET_NAME = 'DD20Mapping'
+    DD20_KEY_NAME = 'DD20 Name'
+    DD20_VALUE_NAME = 'ETS Name'
+
+    dd20_dataframe = pd.read_excel(io=LIMITS_NAME_FILEPATH, sheet_name=DD20_DATASHEET_NAME)
+    TEST_DICTONARY = {'E_GGG-HHH': 'E_GGGV-HHH'}
+
+    assert code.define_dictonary_from_two_columns_in_a_dataframe(dd20_dataframe,
+                                                                 DD20_KEY_NAME, DD20_VALUE_NAME) == TEST_DICTONARY
 
 
 """
@@ -41,26 +70,7 @@ def test_verify_dataframe_columns():
     assert True
 """
 
-'''
-def test_mrid_map_dict():
-    data = ''
-    test_dict = {'d51269gh-25e0-4a11-b32e-bd0fba7af745': 'EEE-FFF-1',
-                     'd51269gh-25e0-4a11-b32e-bd0fba7af747': 'EEE-FFF-2',
-                     'd51269gh-25e0-4a11-b32e-bd0fba7af751': 'GGG-HHH',
-                     'd51269gh-25e0-4a11-b32e-bd0fba7af757': 'CCC-DDD',
-                     'd51269gh-25e0-4a11-b32e-bd0fba7af759': 'III-ÆØÅ',
-                     'd51269gh-25e0-4a11-b32e-bd0fba7af760': 'ASK-ERS'
-                    }
-    assert my_function(data) == test_dict
 
-
-def test_moat_ets_dd20_map_dict():
-
-    data2 = ''
-    test_dict2 = {'E_GGG-HHH': 'E_GGGV-HHH'}
-
-    assert my_function2(data2) == test_dict2
-'''
 """
 def test_combine_conduct_info_to_pandas():
 
